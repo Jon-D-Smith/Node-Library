4a84854f6510ea7f75c3b878cbac1ca4355551b0
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-08 15:00:31 -0600

    Removed body-parser

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config()
-// }
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -16,7 +16,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+app.use(express.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
